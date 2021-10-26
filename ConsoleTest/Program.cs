c19501b3631bc98bfbc2c2cf6da9ce8fb510dd87
mrx10 <mohamad.reza.amoori99@gmail.com>
2021-10-26 12:23:00 +0330

    Test Project

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer;
+using Accounting.DataLayer.UnitOfWork;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitOfWork db = new UnitOfWork();
+            var List=db.CustomerRepository.GetAllCustomers();
+            foreach (var p in List)
+            {
+                Console.WriteLine($"ID : {p.CustomerID}  Name : {p.FullName} Mobile : {p.Mobile} Email : {p.Emaill} Image : {p.CustomerImage}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
